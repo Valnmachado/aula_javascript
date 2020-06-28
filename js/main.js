d16e4bd3979d20fd7a583b550e7d699ffb251139
Valdemir Machado <valdemir.nmachado@gmail.com>
2020-06-27 23:50:25 -0300

    Atualização da aula de javascript

---
+++ js/main.js
@@ -0,0 +1,157 @@
+
+function clicou(){
+    document.getElementById("agradecimento").innerHTML = "<b>Obrigado por clicar</b>";
+
+    //document.getElementById("agradecimento");
+    //console.log(document.getElementById("agradecimento"));
+
+  // console.log(document.getElementById("agradecimento"));
+  // alert("Obrigado por clicar");
+}
+
+function redirecionar(){
+   // window.open("https://digitalinnovation.one");
+    //window.location.href = "https://digitalinnovation.one";
+}
+
+function trocar(elemento){
+    //document.getElementById("mousemove").innerHTML = "Obrigado por passar o mouse";
+    //alert("Trocar texto");
+    elemento.innerHTML = "Obrigado por passar o mouse";
+
+}
+
+function voltar(elemento){
+    //document.getElementById("mousemove").innerHTML = "Passe o mouse aqui";
+    //alert("Trocar texto");
+
+    elemento.innerHTML = "Passe o mouse aqui";
+}
+
+function load(){
+    alert("Página carregada");
+}
+
+function funcaochange(elemento){
+    console.log(elemento.value)
+}
+
+
+//function redirecionar(){
+//    window.open("https://globallabs.academy");
+//}
+
+/*
+var validar = 0;
+function validaIdade(idade){
+    //var validar;
+    if (idade >= 18){
+        //validar = true
+       return true 
+    }else {
+        //validar = false
+        return false
+    }
+    //return validar;
+}
+
+var idade = prompt("Qual a sua idade?");
+console.log(validaIdade(idade));
+
+*/
+
+/*
+function soma(n1, n2){
+    return n1+n2;
+}
+
+function setReplace(frase, nome, novo_nome){
+    return frase.replace(nome, novo_nome);
+}
+
+alert(soma(5, 10));
+alert(setReplace("Vai Japão", "Japão", "Brasil"));
+
+*/
+
+//var d = new Date();
+//alert(d);
+//alert(d.getDay());
+//alert(d.getMonth()+1);
+
+
+/*
+var count;
+for (count=0; count <=5;count++){
+   // alert(count);
+   console.log(count);
+}
+*/
+
+
+/*
+var count = 0;
+
+while (count <=5){
+    console.log(count);
+    //count = count+1;
+    count++;
+}
+*/
+
+/*
+var idade = prompt("Qual a sua idade");
+
+if (idade >= 18) {
+    alert("maior de idade");
+}    
+else {
+    alert("menor de idade");
+}
+
+*/
+
+
+/*
+var frutas = [{nome:"maça", cor:"vermelha"}, {nome:"uva", cor:"roxa"}]
+console.log(frutas);
+alert(frutas[1].nome);
+*/
+
+/*
+var fruta = {nome:"maça", cor:"vermelha"}
+console.log(fruta.nome);
+alert(fruta.cor);
+*/
+  
+
+// comando replace
+
+//time = "O Japão é o melhor timo do mundo";
+//console.log(time.replace("Japão", "Brasil"));
+
+
+//var lista = ["maça", "Pêra", "laranja"];
+
+//console.log(lista);
+//console.log(lista[1]);
+
+// alert(lista[1])
+
+// lista.push("uva");
+// console.log(lista);
+// lista.pop();
+// console.log(lista);
+
+//console.log(lista.length);
+//console.log(lista.reverse());
+
+//console.log(lista[0]);
+//console.log(lista.toString()[0]);
+
+//console.log(lista.toString());
+//console.log(lista.join(" - "));
+
+
+// alert("Bem vindo " + nome +" "+ idade);
+
